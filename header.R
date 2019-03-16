ab0b6b5188a338c9f5b7781ab64543858d2130f4
vincentchi19 <vincent.chi627@gmail.com>
2019-03-16 00:34:42 -0400

    second time commit

--- header.R
+++ header.R
@@ -6,3 +6,15 @@ library(data.table)
 library(ggplot)
 library(readr)
 library(readxl)
+
+teom<-function(){
+  return(year(Sys.Date())*100+month(Sys.Date()))
+}
+
+eomchg<-function(eom,xx){
+  eomx=12*as.interger(eom/100)+eom%%100
+  prevmox=eomx+xx
+  prevmo=100*as.integer((prevmox-1)/12)+(prevmox-1)%%12+1
+  
+  return(prevmo)
+}
